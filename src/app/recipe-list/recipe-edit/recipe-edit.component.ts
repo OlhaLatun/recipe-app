274493770e42592c0646c48ec7599c75e48d453e
Olya Latun <olatu@softserveinc.com>
2023-02-06 18:57:31 +0200

    provided logic to fill out form with data if in edit mode

--- src/app/recipe-list/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe-list/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -9,34 +10,69 @@ import { RecipeService } from '../recipe.service';
 })
 export class RecipeEditComponent {
   recipeForm: FormGroup;
+  editModeItemId: number;
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(
+    private recipeService: RecipeService,
+    private route: ActivatedRoute
+  ) {}
 
   onSubmit() {
+    if (this.editModeItemId) {
+      this.updateRecipe(this.editModeItemId);
+    } else {
+      this.addNewRecipe();
+    }
+
+    this.recipeForm.reset();
+  }
+
+  updateRecipe(id) {
+    console.log(id);
+  }
+
+  filloutTheForm(id: number) {
+    // form doesnt fill out ingredients + how to update recipe
+    const recipeToEdit = this.recipeService.recipes.find((r) => r.id === id);
+    this.recipeForm.setValue({
+      name: recipeToEdit.name,
+      description: recipeToEdit.description,
+      imgPath: recipeToEdit.imagePath,
+      ingredients: recipeToEdit.ingredients,
+    });
+  }
+
+  addNewRecipe() {
     const newRecipe = new Recipe(
       this.recipeService.recipes.length + 1,
       this.recipeForm.value.name,
       this.recipeForm.value.description,
       this.recipeForm.value.imgPath,
       this.recipeForm.value.ingredients
-    )
+    );
 
-    this.recipeService.addRecipe(newRecipe)
-    this.recipeForm.reset()
+    this.recipeService.addRecipe(newRecipe);
   }
 
   ngOnInit() {
+    this.editModeItemId = +this.route.snapshot.params['id'];
+
     this.recipeForm = new FormGroup({
       name: new FormControl('', Validators.required),
       description: new FormControl('', Validators.required),
       imgPath: new FormControl('', Validators.required),
       ingredients: new FormArray([]),
     });
+
+    if (this.editModeItemId) {
+      this.filloutTheForm(this.editModeItemId);
+    }
   }
 
   get controls() {
     return (this.recipeForm.get('ingredients') as FormArray).controls;
   }
+
   addIngredientForm() {
     const control = new FormGroup({
       name: new FormControl('', Validators.required),
